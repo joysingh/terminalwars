5727714b91f358ce07904effc26a7beff39cb5d0
ousmane.amadou <ousmane.amadou89@gmail.com>
2017-01-21 11:21:13 -0500

    Added File and Folder Classes

---
+++ Terminal Wars/Assets/Scripts/File.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class File
+{
+	public string name; // Name of File.
+
+	public File()
+	{
+
+	}
+}
---
+++ Terminal Wars/Assets/Scripts/Folder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Folder
+{
+	public string name; // Name of folder.
+	//public List<SysItem> items; // List of items in current folder including sub folders and hidden data.
+
+	public Folder()
+	{
+		name = "New Folder";
+	}
+
+	public Folder(string n)
+	{
+		name = n;
+	}
+
+	public void addFile(File f)
+	{
+
+	}
+}
