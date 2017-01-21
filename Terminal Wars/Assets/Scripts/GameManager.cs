2afc68ae00c6249f768599ad1b97d65d9b7b1bfd
ousmane.amadou <ousmane.amadou89@gmail.com>
2017-01-21 13:23:59 -0500

    More functionality added

---
+++ Terminal Wars/Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour 
+{
+	public GameObject[] terminal;
+	public string _currentdirectory;
+	public int _currentline;
+	// Use this for initialization
+	void Start () 
+	{
+		terminal[0].GetComponent<Text>().text = "Welcome to Terminaltor!";
+		terminal[1].GetComponent<Text>().text = "Your mission should you choose to accept it is...";
+		terminal[2].GetComponent<Text>().text = "Fuck around to the MAX!";
+		terminal[3].GetComponent<Text>().text = "Ousmanes-MacBook-Pro:terminalwars QuanticSymphonia$";
+		_currentline = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyUp(KeyCode.Return))
+		{
+			int _nextline = _currentline + 1;
+			terminal [_nextline].GetComponent<Text> ().text = "Ousmanes-MacBook-Pro:terminalwars QuanticSymphonia$";
+			_currentline += 1;
+		}
+		if (Input.GetKeyUp (KeyCode.Backspace)) 
+		{
+			terminal [_currentline].GetComponent<Text> ().text
+			= (terminal [_currentline].GetComponent<Text> ().text).Substring (0, terminal [_currentline].GetComponent<Text> ().text.Length - 1);
+
+			_currentline -= 1;
+		}
+
+		else if (Input.inputString != "") 
+		{
+			terminal[_currentline].GetComponent<Text>().text += Input.inputString;
+		}
+
+
+		
+	}
+
+	// Creates a brand new file System For The Game!
+	void initializeGame()
+	{
+
+	}
+
+	//
+	void input() 
+	{
+
+	}
+}
